ef59ce9ea9b92addbccd044d58b1ba51b0ccdbe6
Mitchell <mlrotenberry99@yahoo.com>
2021-03-22 00:31:12 -0500

    Added jumping and ground detection

--- CSCI370Lab5/Assets/Scripts/PlayerMove.cs
+++ CSCI370Lab5/Assets/Scripts/PlayerMove.cs
@@ -10,16 +10,38 @@ public class PlayerMove : MonoBehaviour
 
     public float gravity = -9.81f;
 
+    
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
     private Vector3 velocity;
+    private bool isGrounded;
+
+    public float jumpHeight = 3f;
+
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
 
         velocity.y += gravity * Time.deltaTime;
 
