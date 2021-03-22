f6a95173613e21003078691d53cf2b4ba899a31e
olivia <larsonog@hendrix.edu>
2021-03-21 23:17:04 -0500

    added 3d model of zombie

--- CSCI370Lab5/Assets/Attributions.cs
+++ CSCI370Lab5/Assets/Attributions.cs
@@ -1,3 +1,4 @@
 //Snowflake: https://pixabay.com/illustrations/snowflake-snow-winter-blue-flake-2910087/
 //Heart: https://opengameart.org/content/heart-1
 //Border: https://github.com/Brackeys/Health-Bar/blob/master/Health%20Bar/Assets/Sprites/Bar.png
+// trees : https://opengameart.org/content/low-poly-tree-1
