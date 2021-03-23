26960bd42326ec2fe0ca75585f25e03a8f103fa0
Jonathon <jonathonq02@gmail.com>
2021-03-23 13:30:05 -0500

    I didn't stage the last changes apparently

--- CSCI370Lab5/Assets/Scripts/Player.cs
+++ CSCI370Lab5/Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
@@ -8,10 +10,12 @@ public class Player : MonoBehaviour
     public int currentHealth;
     public HealthBar healthbar;
     public GameObject deathEffect;
+    public GameObject coldImage;
 
     public int minCold = 0;
     public int currentCold;
     public ColdBar coldBar;
+    public bool isCold = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,7 +32,7 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            TakeDamage(20);
+            TakeDamage(10);
         }
     }
 
@@ -44,10 +48,44 @@ public class Player : MonoBehaviour
             Die();
         }else if(currentCold == 100)
         {
+            if (!isCold)
+            {
+                Cold();
+                isCold = true;
+            }
             TakeDamage(10);
-        }
+        } else
+		{
+            if(isCold)
+			{
+                NotCold();
+                isCold = false;
+			}
+		}
     }
+    // Enables or disables a chilled border
+    IEnumerator ColdColorLerp(Color endValue, float duration)
+    {
+        float time = 0;
+        Image sprite = coldImage.GetComponent<Image>();
+        Color startValue = sprite.color;
 
+        while (time < duration)
+        {
+            sprite.color = Color.Lerp(startValue, endValue, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        sprite.color = endValue;
+    }
+    public void Cold()
+    {
+        StartCoroutine(ColdColorLerp(new Color(1, 1, 1, 1), 2));
+    }
+    public void NotCold()
+    {
+        StartCoroutine(ColdColorLerp(new Color(0, 0, 0, 0), 2));
+    }
 
     void Die()
     {
@@ -55,13 +93,4 @@ public class Player : MonoBehaviour
         Destroy(gameObject);
 
     }
-
-    private void OnControllerColliderHit(ControllerColliderHit hit)
-    {
-        if (hit.gameObject.CompareTag("Fire"))
-        {
-            Debug.Log("work");
-        }
-    }
-
 }
